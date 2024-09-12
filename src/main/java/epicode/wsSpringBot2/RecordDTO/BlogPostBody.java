b03b6ee849979e4bf79c6cd51983056428c82cef
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-09-12 16:26:38 +0200

    aaaa

--- src/main/java/epicode/wsSpringBot2/RecordDTO/BlogPostBody.java
+++ src/main/java/epicode/wsSpringBot2/RecordDTO/BlogPostBody.java
@@ -9,9 +9,11 @@ public record BlogPostBody(
         @NotEmpty(message = "manca la categoria")
         @Size(min = 3, max = 20, message = "la categoria deve avere un minimo di 2 ad un massimo di 20 caratteri")
         String categoria,
+
         @NotEmpty(message = "manca il titolo")
         @Size(min = 3, max = 20, message = "il titolo deve avere un minimo di 2 ad un massimo di 20 caratteri")
         String titolo,
+
         @NotEmpty(message = "manca il contenuto")
         @Size(min = 3, max = 50, message = "il contenuto deve avere un minimo di 2 ad un massimo di 50 caratteri")
         String contenuto,
@@ -20,6 +22,7 @@ public record BlogPostBody(
         @Min(value = 1, message = "il tempo di lettura deve essere almeno 1 minuto")
         @Max(value = 999, message = "il tempo di lettura non può superare i 999 minuti")
         Integer tempoDiLettura,
+
         @NotNull(message = "manca l'id dell'autore")
         UUID autoreID) {
 }
--- src/main/java/epicode/wsSpringBot2/RecordDTO/NewAuthorDTO.java
+++ src/main/java/epicode/wsSpringBot2/RecordDTO/NewAuthorDTO.java
@@ -10,12 +10,15 @@ public record NewAuthorDTO(
         @NotEmpty(message = "manca il nome")
         @Size(min = 2, max = 20, message = "il nome deve avere un minimo di 2 ad un massimo di 20 caratteri")
         String nome,
+
         @NotEmpty(message = "manca il cognome")
         @Size(min = 3, max = 20, message = "il cognome deve avere un minimo di 3 ad un massimo di 20 caratteri")
         String cognome,
+
         @NotEmpty(message = "manca l'email")
         @Size(min = 3, max = 20, message = "l'email deve avere un minimo di 2 ad un massimo di 20 caratteri")
         String email,
+
         @NotNull(message = "manca la data Di Nascita")
         @Size(min = 10, max = 11, message = "la data Di Nascita deve avere un minimo di 10 caratteri con questo formato YYYY-MM-DD")
         LocalDate dataDiNascita) {
